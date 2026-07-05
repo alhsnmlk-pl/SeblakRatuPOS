7a5ef963c4aa6a4bf25edc5874f8b1dcd949fc5c
alhsnmlk-pl <aallhsnmlk@gmail.com>
2026-07-05 19:21:33 +0700

    menambahkan main method agar bisa di run di main class

--- src/posseblakratu/POSSeblakRatu.java
+++ src/posseblakratu/POSSeblakRatu.java
@@ -4,6 +4,11 @@
  */
 package posseblakratu;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import posseblakratu.view.FrameLogin;
+
 /**
  *
  * @author Al
@@ -15,6 +20,17 @@ public class POSSeblakRatu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            UIManager.put("ComboBox.selectionBackground", new java.awt.Color(252, 232, 230)); //memgatur warna selection backround di combo box
+            UIManager.put("ComboBox.selectionForeground", new java.awt.Color(0, 0, 0)); //mengatur warna teks / foreground di selection combo box
+            UIManager.put("Button.arc", 10); //membaut button melengkung
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException unsupportedLookAndFeelException) {
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new FrameLogin().setVisible(true));
     }
     
 }
